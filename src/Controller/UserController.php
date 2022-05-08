c4bd89206bb867d7da873821380569382d8ffefe
alexfj498 <alejfuejur@gmail.com>
2022-05-09 00:36:08 +0200

    Moved assets folder to public folder

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -8,7 +8,7 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class UserController extends AbstractController
 {
-    #[Route('/user/list', name: 'user_list')]
+    #[Route('/user/login', name: 'user_list')]
     public function index(): Response
     {
         return $this->render('user/index.html.twig', [
